b7b79898149f9144b639d01e8ade8944f32a5052
Wama85 <wi_an9@hotmail.com>
2025-10-24 19:55:04 -0400

    se añade spha

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,6 +59,7 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
+    implementation("androidx.core:core-splashscreen:1.0.1")
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
@@ -115,4 +116,3 @@ sentry {
     // disable if you don't want to expose your sources
     includeSourceContext.set(true)
 }
-
